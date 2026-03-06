9f8f8965eb25d754f0f5b714c20e63dba015d53c
Jesus Betancourt <jbeta02@gmail.com>
2026-03-05 21:35:34 -0800

    remove ros2 control for now. add lidar fixes

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ install joint state publisher gui
 sudo apt install ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-gazebo-ros2-control
 
 source install/setup.bash
+colcon build --symlink-install
 
 running:
 ros2 launch warehouse_robot rsp.launch.py use_sim_time:=true
@@ -18,3 +19,4 @@ testing:
 to teleop robot: ros2 run teleop_twist_keyboard teleop_twist_keyboard
 to test ros2_control setup: ros2 control list_hardware_interfaces
 use if publishing to diff_cont/cmd_vel_unstamped: ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstamped
+open rviz: rviz2 -d src/warehouse_robot/config/laser_scan.rviz
--- dev_ws/src/warehouse_robot/launch/launch_sim.launch.py
+++ dev_ws/src/warehouse_robot/launch/launch_sim.launch.py
@@ -23,10 +23,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -47,12 +50,22 @@ def generate_launch_description():
         arguments=["joint_broad"]
     )
 
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", "src/warehouse_robot/config/laser_scan.rviz"],
+        parameters=[{"use_sim_time": True}],
+        output="screen",
+    )
+
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        # diff_drive_spawner,
+        # joint_broad_spawner,
+        rviz_node
     ])
